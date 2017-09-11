123202a204db84ed7da23aa5cf6787852680e81e
lhyla <lhylakontakt@gmail.com>
2017-09-11 22:57:19 +0200

    Added correct last name validation

--- src/com/lhyla/CustomerController.java
+++ src/com/lhyla/CustomerController.java
@@ -2,15 +2,26 @@ package com.lhyla;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder){
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	
 	
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
@@ -23,6 +34,7 @@ public class CustomerController {
 			@Valid @ModelAttribute("customer")Customer theCustomer,
 			BindingResult theBindingResult ){
 		
+		System.out.println("Last name: |" + theCustomer.getLastName() + "|");
 		if(theBindingResult.hasErrors()){
 			return "customer-form";
 		}
